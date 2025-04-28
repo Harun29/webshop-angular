318377246b3ef7e12254958a9ec6d9867224bac1
Harun29 <ibrahimovic.harun01@gmail.com>
2025-04-28 16:23:03 +0200

    Enhance SwipeDownDirective test by adding ElementRef mock for instance creation

--- src/app/directives/swipe-down.directive.spec.ts
+++ src/app/directives/swipe-down.directive.spec.ts
@@ -1,8 +1,10 @@
 import { SwipeDownDirective } from './swipe-down.directive';
+import {ElementRef} from '@angular/core';
 
 describe('SwipeDownDirective', () => {
   it('should create an instance', () => {
-    const directive = new SwipeDownDirective();
+    const mockElementRef = jasmine.createSpyObj('ElementRef', ['nativeElement']);
+    const directive = new SwipeDownDirective(mockElementRef as ElementRef);
     expect(directive).toBeTruthy();
   });
 });
